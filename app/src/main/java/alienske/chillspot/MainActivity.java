b835c543e1628097003f04cc6364906022a55785
Sylvia <sylviakieha@gmail.com>
2015-07-22 23:46:01 +0300

    modified facebook button

--- app/src/main/java/alienske/chillspot/MainActivity.java
+++ app/src/main/java/alienske/chillspot/MainActivity.java
@@ -1,17 +1,27 @@
 package alienske.chillspot;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), One.class);
+                startActivity(intent);
+            }
+        });
+    }
+
     }
 
 
-}
