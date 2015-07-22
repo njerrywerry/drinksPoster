903d9705921a5ba4c61c3b626e6f88d6b575d13b
Sylvia <sylviakieha@gmail.com>
2015-07-22 19:37:49 +0300

    added splash screen

--- app/src/main/java/alienske/chillspot/Splash.java
+++ app/src/main/java/alienske/chillspot/Splash.java
@@ -3,6 +3,7 @@ package alienske.chillspot;
 /**
  * Created by njerry on 7/21/15.
  */
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
@@ -11,9 +12,29 @@ public class Splash extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.splash);
 
+        Thread timerThread = new Thread() {
+         public void run (){
+             try {
+                 sleep(3000);
+             }
+             catch(InterruptedException e) {
+                 e.printStackTrace();
+                 }
+             finally {
+                 Intent i = new Intent(getBaseContext(), MainActivity.class);
+                 startActivity(i);
+             }
+         }
+        };
+        timerThread.start();
+    }
+@Override
+    protected void onPause() {
+    super.onPause();
+    finish();
+}
 
 }
 
