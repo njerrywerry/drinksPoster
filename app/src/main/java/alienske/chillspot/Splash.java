ff88ee8fb6791d312a25977af8a4ccf17b69d36f
Sylvia <sylviakieha@gmail.com>
2015-07-22 11:32:00 +0300

    commited Drinks Poster Android Studio project

---
+++ app/src/main/java/alienske/chillspot/Splash.java
@@ -0,0 +1,19 @@
+package alienske.chillspot;
+
+/**
+ * Created by njerry on 7/21/15.
+ */
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class Splash extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
+
