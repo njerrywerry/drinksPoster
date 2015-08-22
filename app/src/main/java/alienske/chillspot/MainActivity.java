df623e37f5ea68cb268a5ef169df933a86a6ce58
Sylvia <sylviakieha@gmail.com>
2015-08-23 02:48:05 +0300

    incorporated maps into the app

--- app/src/main/java/alienske/chillspot/MainActivity.java
+++ app/src/main/java/alienske/chillspot/MainActivity.java
@@ -1,8 +1,8 @@
 package alienske.chillspot;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,10 +14,10 @@ Button button;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button)findViewById(R.id.button);
+        button = (Button)findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getBaseContext(), One.class);
+                Intent intent = new Intent(getBaseContext(), MapsActivity.class);
                 startActivity(intent);
             }
         });
